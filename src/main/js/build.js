e94522770897af1275e2a8ced7e93a5129265b2e
Anton Golub <antongolub@antongolub.com>
2023-03-21 22:47:12 +0300

    feat: replace sequent flow with concurrent
    BREAKING CHANGE: concurrent flow might cause throttling issues

--- src/main/js/build.js
+++ src/main/js/build.js
@@ -1,18 +1,20 @@
 import {traverseDeps} from './deps.js'
 import {fetchPkg} from './npm.js'
-import {runHook} from './processor.js'
+import {runCmd} from './processor.js'
 
-export const build = async (pkg, packages) => {
+export const build = async (pkg, packages, run = runCmd) => {
   if (pkg.built) return true
 
   await traverseDeps(pkg, packages, async (_, {pkg}) => build(pkg, packages))
 
   const {config} = pkg
 
-  if (pkg.changes.length === 0 && config.npmFetch) await fetchPkg(pkg)
+  if (pkg.changes.length === 0 && config.npmFetch) {
+    await fetchPkg(pkg)
+  }
 
   if (!pkg.fetched && config.buildCmd) {
-    await runHook(pkg, 'buildCmd')
+    await run(pkg, 'buildCmd')
   }
 
   pkg.built = true
